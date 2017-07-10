8537cfc1d4bbaa51f7c822ef18e36865a1d15bfd
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-10 13:01:36 +0200

    Added a new ItemStack

---
+++ src/io/github/russianmushroom/item/Stack.java
@@ -0,0 +1,33 @@
+package io.github.russianmushroom.item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+
+public class Stack {
+
+	private int quantity = 0;
+	
+	private Material type = Material.AIR;
+	
+	private short durability = 0;
+	
+	private ItemMeta meta;
+	
+	private Map<Enchantment, Integer> entchantments = new HashMap<>();
+	
+	
+	public Stack(ItemStack iStack) {
+		if(iStack != null) {
+			this.quantity = iStack.getAmount();
+			this.type = iStack.getType();
+			this.durability = iStack.getDurability();
+		}
+	}
+	
+}
