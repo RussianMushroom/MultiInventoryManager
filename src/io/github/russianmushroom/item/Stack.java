2424fe559a1c5acce9d9699bc0715f79627bf140
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-11 19:12:38 +0200

    Improved ItemStack, used for testing default ItemMeta

--- src/io/github/russianmushroom/item/Stack.java
+++ src/io/github/russianmushroom/item/Stack.java
@@ -2,11 +2,17 @@ package io.github.russianmushroom.item;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
 
 
 public class Stack extends Object {
@@ -17,10 +23,9 @@ public class Stack extends Object {
 	
 	private short durability = 0;
 	
-	private ItemMeta meta;
+	private String metaTags = "";
 	
 	private Map<Enchantment, Integer> entchantments = new HashMap<>();
-	
 	
 	public Stack(ItemStack iStack) {
 		if(iStack != null) {
@@ -28,7 +33,15 @@ public class Stack extends Object {
 			this.type = iStack.getType();
 			this.durability = iStack.getDurability();
 			this.entchantments = iStack.getEnchantments();
+			/*
+			if(iStack.getItemMeta() instanceof BookMeta) {
+				
+			} else */if (iStack.hasItemMeta())
+				metaTags = MetaCompress.compressMetaData(iStack);
 		}
+		
+		
+		Bukkit.broadcastMessage(metaTags);
 	}
 	
 }
