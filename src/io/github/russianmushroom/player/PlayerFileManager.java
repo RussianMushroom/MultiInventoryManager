0e1c92ce90c2a942210c0ffe2f73722791e38e0d
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-12 07:34:36 +0200

    Generalised PlayerFileManager

--- src/io/github/russianmushroom/player/PlayerFileManager.java
+++ src/io/github/russianmushroom/player/PlayerFileManager.java
@@ -24,100 +24,37 @@ import io.github.russianmushroom.yaml.BaseYAML;
  */
 public class PlayerFileManager {
 
-	private static Map<String, Map<String, Object>> obj = Collections.synchronizedMap(new HashMap<>());
-	private static Map<String, Object> data = Collections.synchronizedMap(new HashMap<>());
-	
-	private static PlayerManager pManager;
-	
-	private static File playerData;
-	
-	private static final Yaml yaml = new Yaml();
-
 	/**
-	 * Get player's gamemode and save player data accordingly
-	 * @param pM
+	 * Central handling. Deals with saving and loading player data
+	 * @param pManager
+	 * @param saving
 	 * @throws IOException
 	 */
-	public static void save(PlayerManager pM) throws IOException {
-		
-		pManager = pM;
-		
-		// Create user dataFile
-		playerData = new File(BaseYAML.getPlayerFolder() 
-				+ File.separator 
-				+ pManager.getPlayerUUID().toString() 
-				+ ".yml");
-		
-		if(!playerData.exists())
-			createDataFile(playerData);
-		
+	public static void handle(PlayerManager pManager, boolean saving) throws IOException {
 		switch (pManager.getPlayer().getGameMode()) {
 		case ADVENTURE:
-			if(BaseYAML.getData(BaseYAML.CONFIG).get("adventure").equals("true"))
-				addEntry(GameMode.ADVENTURE);
+			if(BaseYAML.getData(BaseYAML.CONFIG).get("adventure").equals("true")) {
+				if(saving)
+					SavePlayerData.save(pManager, GameMode.ADVENTURE);
+			}
 			break;
 		case CREATIVE:
-			addEntry(GameMode.CREATIVE);
+			if(saving)
+				SavePlayerData.save(pManager, GameMode.CREATIVE);
 			break;
 		case SPECTATOR:
-			if(BaseYAML.getData(BaseYAML.CONFIG).get("spectator").equals("true"))
-				addEntry(GameMode.SPECTATOR);
+			if(BaseYAML.getData(BaseYAML.CONFIG).get("spectator").equals("true")) {
+				if(saving)
+					SavePlayerData.save(pManager, GameMode.SPECTATOR);
+			}
 			break;
 		case SURVIVAL:
-			addEntry(GameMode.SURVIVAL);
+			if(saving)
+				SavePlayerData.save(pManager, GameMode.SURVIVAL);
 			break;
 		}
 	}
 	
-	/**
-	 * Create player file with UUID as name. Populate with base template.
-	 * @param basePath
-	 * @param playerUUID
-	 * @throws IOException
-	 */
-	private static void createDataFile(File file) throws IOException {
-		file.createNewFile();
-		populateDataFile(file);
-	}
 	
-	/**
-	 * Saves player data to yml file
-	 * @param gMode
-	 * @throws IOException
-	 */
-	@SuppressWarnings("unchecked")
-	private static void addEntry(GameMode gMode) throws IOException {
-		
-		data.put("playerInventory", pManager.getPlayerInv().orElse(Arrays.asList("empty")));
-		data.put("playerHealth", pManager.getPlayerHealth());
-		data.put("playerXP", pManager.getPlayerXP());
-		data.put("playerLvl", pManager.getPlayerLvl());
-		
-		
-		obj = (Map<String, Map<String, Object>>) yaml.load(new FileInputStream(playerData));
-		obj.replace(gMode.toString(), data);
-		
-		FileWriter writer = new FileWriter(playerData);
-		
-		yaml.dump(obj, writer);
-		
-	}
-	
-	/**
-	 * Add base template to {@link dataFile}
-	 * @param dataFile
-	 */
-	private static void populateDataFile(File dataFile) throws IOException {
-		
-		obj.put(GameMode.SURVIVAL.toString(), new HashMap<>());
-		obj.put(GameMode.CREATIVE.toString(), new HashMap<>());
-		obj.put(GameMode.SPECTATOR.toString(), new HashMap<>());
-		obj.put(GameMode.ADVENTURE.toString(), new HashMap<>());
-		
-		FileWriter writer = new FileWriter(dataFile);
-		
-		yaml.dump(obj, writer);
-		
-	}
 	
 }
