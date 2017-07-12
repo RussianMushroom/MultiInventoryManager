a98f0719d75fbbb3d1c87f771ada9c825a6344ed
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-12 21:43:05 +0200

    Cleaned up.
    Still work to be done on other Metas.

--- src/io/github/russianmushroom/item/Stack.java
+++ src/io/github/russianmushroom/item/Stack.java
@@ -1,18 +1,7 @@
 package io.github.russianmushroom.item;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
-import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionData;
 
 
 public class Stack extends Object {
@@ -25,29 +14,26 @@ public class Stack extends Object {
 	
 	private String metaTags = "";
 	
-	private Map<Enchantment, Integer> entchantments = new HashMap<>();
+	// private Map<Enchantment, Integer> entchantments = new HashMap<>();
 	
 	public Stack(ItemStack iStack) {
 		if(iStack != null) {
 			this.quantity = iStack.getAmount();
 			this.type = iStack.getType();
 			this.durability = iStack.getDurability();
-			this.entchantments = iStack.getEnchantments();
+			// this.entchantments = iStack.getEnchantments();
 			/*
 			if(iStack.getItemMeta() instanceof BookMeta) {
 				
 			} else */if (iStack.hasItemMeta())
 				metaTags = MetaCompress.compressMetaData(iStack);
 		}
-		
-		
-		Bukkit.broadcastMessage(metaTags);
 	}
 	
 	@Override
 	public String toString() {
 		return String.format(
-				"Quantity:%s\nType:%s\nDurability:%s\nMeta:%s",
+				"Quantity:%sType:%sDurability:%sMeta:%s",
 				quantity,
 				type,
 				durability,
