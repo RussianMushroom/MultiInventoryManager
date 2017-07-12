4982d33ee580dab988d2dfb50220679f53f16258
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-12 21:42:42 +0200

    Removed tests and added proper listeners.

--- src/io/github/russianmushroom/listener/PlayerListener.java
+++ src/io/github/russianmushroom/listener/PlayerListener.java
@@ -1,28 +1,16 @@
 package io.github.russianmushroom.listener;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.ItemStack;
 
-import io.github.russianmushroom.item.MetaCompress;
-import io.github.russianmushroom.item.MetaDecompress;
-import io.github.russianmushroom.item.Stack;
-import io.github.russianmushroom.player.PlayerFileManager;
+import io.github.russianmushroom.player.LoadPlayerData;
 import io.github.russianmushroom.player.PlayerManager;
 import io.github.russianmushroom.player.SavePlayerData;
-import io.github.russianmushroom.yaml.BaseYAML;
 
 
 /**
@@ -39,6 +27,39 @@ public class PlayerListener implements Listener{
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		// Update files
+		try {
+			LoadPlayerData.load(
+					new PlayerManager(event.getPlayer()), 
+					event.getPlayer().getGameMode());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Check the previous GameMode and save and load new GameMode
+	 * @param event
+	 */
+	@EventHandler
+	public void onPlayerGameModeChange(PlayerGameModeChangeEvent event) {
+		// Save previous GameMode
+		try {
+			SavePlayerData.save(
+					new PlayerManager(event.getPlayer()), 
+					event.getPlayer().getGameMode());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Load data for next GameMode
+		try {
+			LoadPlayerData.load(
+					new PlayerManager(event.getPlayer()),
+					event.getNewGameMode());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	/**
@@ -48,24 +69,14 @@ public class PlayerListener implements Listener{
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		// Update files
-	}
-	
-	// Used for testing purposes
-	@EventHandler
-	public void onPlayerEggThrow(PlayerEggThrowEvent event) {
-		for(ItemStack s : event.getPlayer().getInventory().getContents()) {
-			if(s != null) {
-				Stack stack = new Stack(s);
-				Bukkit.broadcastMessage(stack.toString());
-			}
-		}
-		PlayerManager pM = new PlayerManager(event.getPlayer());
 		try {
-			PlayerFileManager.handle(pM, true);
+			SavePlayerData.save(
+					new PlayerManager(event.getPlayer()), 
+					event.getPlayer().getGameMode());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
 	
 }
