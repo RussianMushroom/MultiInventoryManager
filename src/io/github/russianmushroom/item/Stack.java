ef21dba7950e3515fd54ee1b465e3b393234ae8b
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-13 20:59:30 +0200

    Added checks. Still to be fixed.

--- src/io/github/russianmushroom/item/Stack.java
+++ src/io/github/russianmushroom/item/Stack.java
@@ -1,9 +1,14 @@
 package io.github.russianmushroom.item;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 
 
@@ -16,17 +21,20 @@ public class Stack extends Object {
 	
 	private short durability = 0;
 	
-	private String metaTags = "";
+	private String metaTags = null;
+	private String enchantTags= null;
 	
+	private Map<Enchantment, Integer> entchantments = Collections.synchronizedMap(new HashMap<>());
 	
-	// private Map<Enchantment, Integer> entchantments = new HashMap<>();
+	private List<String> individualComp;
 	
 	public Stack(ItemStack iStack) {
 		if(iStack != null) {
 			this.quantity = iStack.getAmount();
 			this.type = iStack.getType();
 			this.durability = iStack.getDurability();
-			// this.entchantments = iStack.getEnchantments();
+			this.entchantments = iStack.getEnchantments();
+			this.enchantTags = MetaCompress.compressEnchantments(entchantments);
 			/*
 			if(iStack.getItemMeta() instanceof BookMeta) {
 				
@@ -40,12 +48,18 @@ public class Stack extends Object {
 	 * @param iStack
 	 */
 	public Stack(String iStack) {
-		List<String> individualComp = Arrays.asList(iStack.split("::"));
-		
-		quantity = Integer.parseInt(individualComp.get(0));
-		type = Material.getMaterial(individualComp.get(1));
-		durability = Short.parseShort(individualComp.get(2));
-		metaTags = individualComp.get(3);
+		individualComp = Arrays.asList(iStack.split(":"));
+		Bukkit.broadcastMessage(individualComp.size() + "");
+		if(has(0))
+			quantity = Integer.parseInt(individualComp.get(0));
+		if(has(1))
+			type = Material.getMaterial(individualComp.get(1));
+		if(has(2))
+			durability = Short.parseShort(individualComp.get(2));
+		if(has(3))
+			metaTags = individualComp.get(3);
+		if(has(4))
+			enchantTags = individualComp.get(4);
 	}
 	
 	public ItemStack toItemStack() {
@@ -54,6 +68,7 @@ public class Stack extends Object {
 		iStack.setAmount(quantity);
 		iStack.setDurability(durability);
 		iStack = MetaDecompress.decompressMetaData(iStack, metaTags);
+		iStack.addEnchantments(MetaDecompress.decompressEnchantments(enchantTags));
 		
 		return iStack;
 	}
@@ -61,12 +76,17 @@ public class Stack extends Object {
 	@Override
 	public String toString() {
 		return String.format(
-				"::%s::%s::%s::%s",
+				"%s:%s:%s:%s:%s:",
 				quantity,
 				type,
 				durability,
-				metaTags
+				metaTags,
+				enchantTags
 				);
+	}
+	
+	private boolean has(int i) {
+		return (individualComp.get(i) == null | individualComp.get(i).equals("null")) ? false : true;
 	}
 	
 }
