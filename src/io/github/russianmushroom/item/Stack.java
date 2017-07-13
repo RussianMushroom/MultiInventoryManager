ae12f0236c996299bf850053d308354e89fcd9fb
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-13 08:16:46 +0200

    Added new methods to create ItemStack from raw string.
    TODO: Implement slots!

--- src/io/github/russianmushroom/item/Stack.java
+++ src/io/github/russianmushroom/item/Stack.java
@@ -1,5 +1,8 @@
 package io.github.russianmushroom.item;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -7,12 +10,14 @@ import org.bukkit.inventory.ItemStack;
 public class Stack extends Object {
 
 	private int quantity = 0;
+	private int slot = 0;
 	
 	private Material type = Material.AIR;
 	
 	private short durability = 0;
 	
 	private String metaTags = "";
+	
 	
 	// private Map<Enchantment, Integer> entchantments = new HashMap<>();
 	
@@ -30,10 +35,33 @@ public class Stack extends Object {
 		}
 	}
 	
+	/**
+	 * Receives raw input from save file and converts to Stack
+	 * @param iStack
+	 */
+	public Stack(String iStack) {
+		List<String> individualComp = Arrays.asList(iStack.split("::"));
+		
+		quantity = Integer.parseInt(individualComp.get(0));
+		type = Material.getMaterial(individualComp.get(1));
+		durability = Short.parseShort(individualComp.get(2));
+		metaTags = individualComp.get(3);
+	}
+	
+	public ItemStack toItemStack() {
+		ItemStack iStack = new ItemStack(type);
+		
+		iStack.setAmount(quantity);
+		iStack.setDurability(durability);
+		iStack = MetaDecompress.decompressMetaData(iStack, metaTags);
+		
+		return iStack;
+	}
+	
 	@Override
 	public String toString() {
 		return String.format(
-				"Quantity:%sType:%sDurability:%sMeta:%s",
+				"::%s::%s::%s::%s",
 				quantity,
 				type,
 				durability,
