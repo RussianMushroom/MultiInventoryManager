1133ebf410c9e31d9e90e015806e4fdb14ac27d8
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-13 08:17:18 +0200

    Now thread-safe

--- src/io/github/russianmushroom/player/LoadPlayerData.java
+++ src/io/github/russianmushroom/player/LoadPlayerData.java
@@ -2,13 +2,17 @@ package io.github.russianmushroom.player;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.inventory.ItemStack;
 
+import io.github.russianmushroom.item.Stack;
 import io.github.russianmushroom.yaml.BaseYAML;
 /**
  * Load player data from file and apply it.
@@ -19,7 +23,7 @@ public class LoadPlayerData {
 	
 	private static Map<String, Object> playerInventory = Collections.synchronizedMap(new HashMap<>());
 
-	public static void load(PlayerManager pManager, GameMode gMode) throws IOException {
+	public synchronized static void load(PlayerManager pManager, GameMode gMode) throws IOException {
 		loadPlayerInventory(pManager, gMode);
 		
 	}
@@ -32,9 +36,9 @@ public class LoadPlayerData {
 				+ ".yml"))
 				.get(gMode.toString());
 		
-		Bukkit.broadcastMessage(gMode.toString());
-		
+		// Set data to player
 		setPlayerData(pManager);
+		setPlayerInventory(pManager);
 	}
 	
 	private static void setPlayerData(PlayerManager pManager) {
@@ -45,6 +49,26 @@ public class LoadPlayerData {
 				Float.parseFloat(playerInventory.get("playerXP").toString()));
 		pManager.getPlayer().setLevel(
 				Integer.parseInt(playerInventory.get("playerLvl").toString()));
+		
+	}
+	
+	/**
+	 * Split the saved inventory into it's components
+	 * <ul>
+	 * <li><code>.split("#")</code>: to get individual items</li>
+	 * <li><code>.split("::")</code>: to get individual components, like Durability, MetaData etc.</li>
+	 * <li>get MetaTags (Last in list) and decompress it</li>
+	 * @param pManager
+	 */
+	private static void setPlayerInventory(PlayerManager pManager) {
+		String playerInv = playerInventory.get("playerInventory").toString();
+		List<String> individualItems = Arrays.asList(playerInv.split("#"));
+		
+		individualItems.parallelStream()
+			.forEach(rawItem -> {
+				Stack stack = new Stack(rawItem);
+				ItemStack iStack = stack.toItemStack();
+			});
 		
 	}
 	
