62d9b0d013dfba4c37ba18521fe504f56e455a31
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-20 10:40:00 +0200

    Beautified

--- src/io/github/russianmushroom/MultiInventoryManager.java
+++ src/io/github/russianmushroom/MultiInventoryManager.java
@@ -9,10 +9,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import io.github.russianmushroom.command.DeleteData;
+import io.github.russianmushroom.files.DeleteData;
+import io.github.russianmushroom.files.LoadDefaults;
 import io.github.russianmushroom.listener.PlayerListener;
+import io.github.russianmushroom.player.PlayerFileManager;
 import io.github.russianmushroom.player.PlayerManager;
-import io.github.russianmushroom.player.SavePlayerData;
 
 /**
  * Separates the inventories as well as data of players from one another through worlds 
@@ -28,6 +29,11 @@ public class MultiInventoryManager extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		this.log.info(pdfFile.getName() +" v" + pdfFile.getVersion() + " has been enabled!");
+		
+		// Add necessary files
+		LoadDefaults.load();
+
+		// Attach listeners
 		getServer().getPluginManager().registerEvents(new PlayerListener(), this);
 		
 		super.onEnable();
@@ -42,9 +48,10 @@ public class MultiInventoryManager extends JavaPlugin {
 			.parallelStream()
 			.forEach(player -> {
 				try {
-					SavePlayerData.save(new PlayerManager(player), player.getGameMode());
+					PlayerFileManager.handle(
+							new PlayerManager(player.getPlayer()), 
+							true);
 				} catch (IOException e) {
-					
 					e.printStackTrace();
 				}
 			});
@@ -58,14 +65,14 @@ public class MultiInventoryManager extends JavaPlugin {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(label.equalsIgnoreCase("mim")) {
 			if(args.length == 0)
-				sender.sendMessage("Invalid syntax: /mim delete [player name]");
+				sender.sendMessage("Invalid syntax");
 			else if(args.length > 2)
-				sender.sendMessage("Invalid syntax: /mim delete [player name]");
+				sender.sendMessage("Invalid syntax");
 			else {
 				if(args[0].equalsIgnoreCase("delete"))
 					DeleteData.remove(sender, args[1]);
 				else
-					sender.sendMessage("Invalid command.");
+					sender.sendMessage("Invalid command!");
 			}
 				
 				
