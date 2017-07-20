fd53a165672b748314c03b78d870612c22135018
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-20 10:41:02 +0200

    Made use of Optional

--- src/io/github/russianmushroom/yaml/BaseYAML.java
+++ src/io/github/russianmushroom/yaml/BaseYAML.java
@@ -2,13 +2,14 @@ package io.github.russianmushroom.yaml;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Optional;
 
 import org.yaml.snakeyaml.Yaml;
+
+import io.github.russianmushroom.files.LoadDefaults;
 
 /**
  * All base YAML methods
@@ -18,53 +19,28 @@ public class BaseYAML {
 	
 	static boolean saveAdventure = false;
 	static boolean saveSpectator = false;
-	
-	public static final File CONFIG = new File("plugins/MIMconfig.yml");
-	
-	private static File baseFile = new File("plugins/MIM/playerdata");
-	
 	private static Yaml ymal = new Yaml();
 	
 	@SuppressWarnings("unchecked")
-	public static Map<String, Object> getData(File targetFile) throws IOException {
-		return (Map<String, Object>) ymal.load(
-				new FileInputStream(targetFile));
+	public static Optional<Map<String, Object>> getData(File targetFile)
+			throws IOException, FileNotFoundException {
+		return Optional.of((Map<String, Object>) ymal.load(
+				new FileInputStream(targetFile)));
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static Map<String, Map<String, Object>> getAllData(File targetFile) throws IOException {
-		return (Map<String, Map<String, Object>>) ymal.load(
-				new FileInputStream(targetFile));
+	public static Optional<Map<String, Map<String, Object>>> getAllData(File targetFile) 
+			throws IOException, FileNotFoundException {
+		return Optional.of((Map<String, Map<String, Object>>) ymal.load(
+				new FileInputStream(targetFile)));
 	}
 	
-	/**
-	 * This reads the {@link MIMconfig.yml} file in the plugins folder to get the <code>basePath</code>.
-	 * If it is not found, it resorts to a default.
-	 * @return basePath of the files.
-	 */
-	public static Path getPlayerFolder() {
-			try {
-				if(!CONFIG.exists())
-					Logger.getLogger("Minecraft").log(Level.WARNING,
-							String.format("%s was not detected, using default path: %s",
-									CONFIG.toString(),
-									baseFile.toString()));
-				else
-					baseFile = new File(getData(CONFIG).get("basePath").toString());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			// Create directory that is used
-			if(!baseFile.exists())
-				baseFile.mkdirs();
-			
-			return baseFile.toPath();
-	}
 	
 	public static boolean getSaveSpectator() {
 		try {
-			return Boolean.valueOf(getData(CONFIG).get("spectator").toString());
+			if(!getData(LoadDefaults.CONFIG_FILE).isPresent())
+				return saveSpectator;
+			return Boolean.valueOf(getData(LoadDefaults.CONFIG_FILE).get().get("spectator").toString());
 		} catch (IOException e) {
 			return saveSpectator;
 		}
@@ -72,7 +48,9 @@ public class BaseYAML {
 	
 	public static boolean getSaveAdventure() {
 		try {
-			return Boolean.valueOf(getData(CONFIG).get("adventure").toString());
+			if(!getData(LoadDefaults.CONFIG_FILE).isPresent())
+				return saveAdventure;
+			return Boolean.valueOf(getData(LoadDefaults.CONFIG_FILE).get().get("adventure").toString());
 		} catch (IOException e) {
 			return saveAdventure;
 		}
