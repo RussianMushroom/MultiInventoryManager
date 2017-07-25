0bb452c9047b31962f35c3e2d3d3d30e3943595a
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-25 13:05:28 +0200

    Finished Book class.

--- src/io/github/russianmushroom/item/Book.java
+++ src/io/github/russianmushroom/item/Book.java
@@ -1,53 +1,58 @@
 package io.github.russianmushroom.item;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.bukkit.inventory.meta.BookMeta;
 
+import io.github.russianmushroom.files.LoadDefaults;
+
 /**
  * Deal with books and their content.
  * They are to be saved int he same directory as player data under the name:
- * book_md5hash.yml
+ * md5hash (no extension)
  * @author RussianMushroom
  *
  */
-@SuppressWarnings("unused")
 public class Book {
 
 	private String bookTitle;
 	private String bookAuthorName;
 	private List<String> bookPages;
 	
+	
 	public Book(String bookTitle, String bookAuthorName, List<String> bookPages) {
 		this.bookTitle = bookTitle;
 		this.bookAuthorName = bookAuthorName;
 		this.bookPages = bookPages;
-		
-		//Check if 
 	}
 	
-	public static String getHash(File file) throws 
-		NoSuchAlgorithmException, 
-		IOException{
-		MessageDigest md = MessageDigest.getInstance("MD5");
+	public String getHash(String bookContents) {
+		MessageDigest md;
+		String result = "";
+		try {
+			md = MessageDigest.getInstance("MD5");
+			byte[] bytes = bookContents.getBytes();
+			
+			md.update(bytes);
 
-		byte[] fileByte = Files.readAllBytes(file.toPath());
-		md.update(fileByte);
+			BigInteger hash = new BigInteger(1, md.digest());
+			result = hash.toString(16);
 
-		BigInteger hash = new BigInteger(1, md.digest());
-		String result = hash.toString(16);
-
-		while(result.length() < 32) {
-			result = "0" + result;
+			while(result.length() < 32) {
+				result = "0" + result;
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
 		}
 
 		return result;
@@ -57,8 +62,63 @@ public class Book {
 	 * Returns the md5_hash of the book file.
 	 * @return
 	 */
-	public static String toMetaString() {
-		return null;
+	public String toMetaString() {
+		return getHash(compressContents());
+	}
+	
+	/**
+	 * Compress all the book contents to one string
+	 * @return
+	 */
+	private String compressContents() {
+		StringBuilder sBuilder = new StringBuilder();
+		
+		sBuilder.append(bookTitle + "||##");
+		sBuilder.append(bookAuthorName + "||##");
+		
+		bookPages.forEach(page -> {
+			sBuilder.append(page + "##|");
+		});
+		
+		return sBuilder.toString();
+	}
+	
+	/**
+	 * Create the file with the md5_hash part of the name
+	 */
+	public void createFile() {
+		File file = new File(LoadDefaults.playerSaveBooks + File.separator + getHash(compressContents())); 
+		
+		try {
+			FileWriter fWriter = new FileWriter(file);
+			
+			fWriter.write(compressContents());
+			fWriter.flush();
+			fWriter.close();
+		} catch (IOException e) {
+		}
+	
+	}
+	
+	public static String readFile(String md5Hash) {
+		File file = new File(LoadDefaults.playerSaveBooks + File.separator + md5Hash);
+		StringBuilder sBuilder = new StringBuilder();
+		if(file.exists()) {
+			try {
+				BufferedReader bReader = new BufferedReader(new FileReader(file));
+				String line = "";
+				
+				while((line = bReader.readLine()) != null) {
+					sBuilder.append(line);
+				}
+				
+				bReader.close();
+			} catch (IOException e) {
+			}
+			return sBuilder.toString();
+		}
+		
+		return "";
 	}
 	
 	/**
@@ -67,14 +127,13 @@ public class Book {
 	 * @param bookData
 	 * @return
 	 */
-	public static Optional<BookMeta> getBookMeta(Map<String, Object> bookData) {
-		BookMeta bMeta = null;
+	public static Optional<BookMeta> getBookMeta(BookMeta bMeta, String bookData) {
 		if(!bookData.isEmpty()) {
-			bMeta.setTitle(bookData.get("bookTitle").toString());
-			bMeta.setAuthor(bookData.get("bookAuthorName").toString());
+			String[] data = bookData.split("\\|\\|##");
+			bMeta.setTitle(data[0]);
+			bMeta.setAuthor(data[1]);
 			
-			Arrays.asList(bookData.get("bookPages").toString().split("\\~newpage\\~"))
-				.stream()
+			Arrays.asList(data[2].split("##\\|"))
 				.forEach(page -> {
 					bMeta.addPage(page);
 				});
